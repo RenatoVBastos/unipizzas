b07718f67a9e2e28c132c2072892a6de86f64660
Renato Vasconcellos <Renato Vasconcellos>
2016-09-22 19:04:43 -0300

    Inserido o bootstrap e criado o inicio do form de contato

--- resources/views/Templates/main.blade.php
+++ resources/views/Templates/main.blade.php
@@ -4,16 +4,21 @@
 	<title>UniPizzas - @yield('title')</title>
 </head>
 <body>
-		<ul>
-			<li><a href="./">Home</a></li>
+		<nav class="navbar" navbar-default navbar-static-top>
+		<div class="container-fluid">
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="./">Home</a></li>
 			<li><a href="./sabores">Sabores</a></li>
 			<li><a href="./pedido">Faça seu pedido</a></li>
 			<li><a href="./contato">Contato</a></li>
 		</ul>
+		</div>
+		</nav>
 	<div class="container">
 		@yield('conteudo')
 	</div>
-	<footer>
+	<footer >
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<p>Footer</p>
 	</footer>
 </body>
--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,5 +1,52 @@
 @extends('./Templates/main')
 @section('title', 'Contato')
 @section('conteudo')
-    <p>Aqui é o Contato</p>
+    <form class="form-horizontal">
+        <fieldset>
+
+        <legend>Contato</legend>
+
+            <!-- Nome-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Nome :</label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="João da Silva" class="form-control input-md">
+                </div>
+            </div>
+
+            <!-- E-mail-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">E-mail: </label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="joaozinho_bolaDEFOGO@gmail.com" class="form-control input-md">
+                </div>
+            </div>
+
+            <!-- Assunto-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textinput">Assunto: </label>
+                <div class="col-md-4">
+                    <input id="textinput" name="textinput" type="text" placeholder="Digite aqui o assunto" class="form-control input-md">
+                </div>
+            </div>
+
+            <!-- Mensagem -->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="textarea">Mensagem: </label>
+                <div class="col-md-4">
+                    <textarea class="form-control" id="textarea" name="textarea" placeholder="Escreva seu texto aqui"></textarea>
+                </div>
+            </div>
+
+            <!-- Botões -->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="button1id"></label>
+                <div class="col-md-8">
+                    <input type="submit" id="button1id" name="button1id" class="btn btn-success"></input>
+                    <input type="reset" id="button2id" name="button2id" class="btn btn-danger"></input>
+                </div>
+            </div>
+
+        </fieldset>
+    </form>
 @endsection
