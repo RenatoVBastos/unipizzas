4f924f5baf7779b86f799b180327c16a068ec8f2
Renato Vasconcellos <Renato Vasconcellos>
2016-09-24 03:54:19 -0300

    Criado o esqueleto da página sabores

--- resources/views/sabores.blade.php
+++ resources/views/sabores.blade.php
@@ -1,5 +1,141 @@
 @extends('layouts.templates')
 @section('title', 'Sabores')
 @section('conteudo')
-    <p>Aqui são os Sabores.</p>
+    <div class="container">
+
+        <!-- Titulo -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Sabores
+                    <small></small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Quebra de coluna -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Quebra de coluna -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+        </div>
+
+        <!-- Quebra de coluna -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/270x300" alt="">
+                </a>
+                <h3>
+                    <a href="#">Sabor</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Paginação -->
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="#">&laquo;</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#2">2</a>
+                    </li>
+                    <li>
+                        <a href="#3">3</a>
+                    </li>
+                    <li>
+                        <a href="#4">4</a>
+                    </li>
+                    <li>
+                        <a href="#5">5</a>
+                    </li>
+                    <li>
+                        <a href="#">&raquo;</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 @endsection
