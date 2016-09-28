25e070fce2927e0b6babe24e6eeb0f03e6abe627
fernandocarrasco <fernando.carrasco0@gmail.com>
2016-09-28 19:02:55 -0300

    Slideshow e 1ª versão do Navbar arrumados

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,8 @@
     <footer>
         <p>Footer</p>
         <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/isotope.pkgd.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/sabores.js')}}"></script>
     </footer>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,11 @@
     @yield('css')
 	<title>UniPizzas - @yield('title')</title>
 	<link rel="stylesheet" href="{{asset('css/app.css')}}" type="text/css">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 </head>
 <body>
 	@yield('navbar')
+	@yield('slideshow')
 	<div class="container">
 		@yield('conteudo')
 	</div>
