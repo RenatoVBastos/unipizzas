8bd7b1bcc711d5a9ae459e902659914f5727f904
Renato Vasconcellos <renatovbastos@ufrj.br>
2016-09-29 00:28:37 -0300

    Footer e navbar parcialmente completos

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,27 @@
 @section('footer')
-    <footer xmlns="http://www.w3.org/1999/html">
-        <p>Footer</p>
+    <footer xmlns="http://www.w3.org/1999/html" class="page-footer blue-grey darken-3">
         <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('materialize/js/materialize.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/isotope.pkgd.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/sabores.js')}}"></script>
+
+        <div class="container">
+            <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
+
+            <link rel="stylesheet" href="css/style.css">
+            <div class="row">
+                <div class="center">
+                    <h5 class="white-text">Endereço</h5>
+                    <p class="grey-text text-lighten-4">R. Pinto de Figueiredo, 84 - Tijuca, Rio de Janeiro</p>
+                    <p class="grey-text text-lighten-4">Telefone: (21) 99999-9999</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2016 Copyright por UniPizzas
+        </div>
     </footer>
 @endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,13 +1,21 @@
 @section('navbar')
-    <nav class="deep-orange accent-4" >
-        <div class="nav-wrapper container left">
-            <ul class="right">
-                <li><a href="./">Home</a></li>
-                <li><a href="./sabores">Sabores</a></li>
-                <li><a href="./pedido">Faça seu pedido</a></li>
-                <li><a href="./contato">Contato</a></li>
-            </ul>
-            <li href="#" class="brand-logo left">UniPizzas</li>
-        </div>
-    </nav>
+    <div class="navbar-fixed">
+        <nav class="deep-orange darken-3" >
+            <div class="nav-wrapper container left">
+                <ul class="right">
+                    <li><a href="./">Home</a></li>
+                    <li><a href="./sabores">Sabores</a></li>
+                    <li><a href="./pedido">Faça seu pedido</a></li>
+                    <li><a href="./contato">Contato</a></li>
+                </ul>
+                <li href="#" class="brand-logo left">UniPizzas</li>
+            </div>
+            <div class="right">
+                <ul class=" left">
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Registre-se</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 @endsection
