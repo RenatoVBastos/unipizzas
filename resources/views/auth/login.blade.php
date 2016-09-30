3dd3657b94e3649e9896bde2a2a044adf1605ed4
Renato Vasconcellos <renatovbastos@ufrj.br>
2016-09-29 23:58:18 -0300

    Login estilizado, porém cor das bordas verde, precisa de bugfix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container login">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col m3"></div>
+        <div class="col m6">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading"><h5>Login</h5></div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Endereço de e-mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -25,7 +26,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Senha</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -39,23 +40,21 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
+                            <div class="col s12">
+                                <p>
+                                    <input type="checkbox" class="filled-in checkbox-orange" id="remember">
+                                    <label for="remember">Remember me</label>
+                                </p>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group botao-login">
+                            <div class="col-md-8 ">
+                                <button type="submit" class="btn btn-primary deep-orange darken-3">
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
+                                <a class="btn btn-link deep-orange darken-3" href="{{ url('/password/reset') }}">
+                                    Esqueceu sua senha?
                                 </a>
                             </div>
                         </div>
