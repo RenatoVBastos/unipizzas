96196a92780e0741fbb15b3f2d9c3a640bc0d37f
Renato Vasconcellos <renatovbastos@ufrj.br>
2016-10-05 19:39:02 -0300

    Bugfix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,10 @@
 @section('footer')
     <footer xmlns="http://www.w3.org/1999/html" class="page-footer blue-grey darken-3">
         <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('materialize/js/materialize.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/isotope.pkgd.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/sabores.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/modal.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/admin.js')}}"></script>
 
 
         <div class="container">
